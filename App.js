b61ef4cfa9bea02962110f8ed002f51c027c4017
Daniel O Sousa <oliveira.daaaaniel@gmail.com>
2023-05-04 15:00:54 -0300

    The useState hook

--- App.js
+++ App.js
@@ -1,10 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { useState } from "react";
 
 export default function App() {
+  const [ name, setName ] = useState("Shaun");
+  const [ person, setPerson ] = useState({ name: "Mario", age: 40 });
+
+  const pressHandler = () => {
+    setName("Banana");
+    setPerson({ name: "Luigi", age: 45 });
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>My name is { name }!</Text>
+      <Text>His name is { person.name } and his age is { person.age }.</Text>
+      <View style={ styles.buttonContainer }>
+        <Button
+          title="Update state"
+          onPress={ pressHandler }
+        />
+      </View>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +34,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  buttonContainer: {
+    marginTop: 20
+  }
 });
