f195e68a8e171aaf6dbc2d500c4cbd406fbf966f
Daniel O Sousa <oliveira.daaaaniel@gmail.com>
2023-05-27 11:33:08 -0300

    Navigation between screens

--- screens/Home.js
+++ screens/Home.js
@@ -1,11 +1,20 @@
-import { View, Text } from "react-native";
+import { View, Text, Button } from "react-native";
 
 import { globalStyles } from "../styles/global";
 
-function Home() {
+function Home({ navigation }) {
+  const pressHandler = () => {
+    navigation.navigate("ReviewDetails");
+    // navigation.push("ReviewDetails");
+  };
+
   return (
     <View style={ globalStyles.container }>
       <Text style={ globalStyles.title }>Home</Text>
+      <Button
+        title="Go to review details"
+        onPress={ pressHandler }
+      />
     </View>
   )
 }
--- screens/ReviewDetails.js
+++ screens/ReviewDetails.js
@@ -1,11 +1,19 @@
-import { View, Text } from "react-native";
+import { View, Text, Button } from "react-native";
 
 import { globalStyles } from "../styles/global";
 
-function ReviewDetails() {
+function ReviewDetails({ navigation }) {
+  const pressHandler = () => {
+    navigation.goBack();
+  }
+
   return (
     <View style={ globalStyles.container }>
       <Text>Review Details</Text>
+      <Button
+        title="Go back home"
+        onPress={ pressHandler }
+      />
     </View>
   )
 }
